7e1fa6d9947155a1fcc3414b1f268694bf1ec316
MichalekM <MichalekM@VDT-MICHALEKM.tescosw.loc>
2018-07-10 13:33:25 +0200

    data from tiff recognized

--- main/src/main/java/org/extractor/main/App.java
+++ main/src/main/java/org/extractor/main/App.java
@@ -8,6 +8,9 @@ import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import mil.nga.tiff.*;
 
@@ -20,18 +23,48 @@ public class App
     public static void main( String[] args ) throws IOException
     {
     	
-    	File file = new File("world/wc2.0_10m_tavg_01.tif");
+    	//File file = new File("world/wc2.0_10m_tavg_01.tif");
+    	File file = new File("world/rendermap.tiff");
         
         TIFFImage tiffImage = TiffReader.readTiff(file);
         
+        FileDirectory fileDirectory = tiffImage.getFileDirectories().get(0);        
+        Rasters rasters = fileDirectory.readRasters();         
+        ByteBuffer byteBuffer = rasters.getSampleValues()[0];        
+        Set<Float> values = new HashSet<>();
         
         
-        FileDirectory fileDirectory = tiffImage.getFileDirectories().get(0);
-        System.out.println(fileDirectory.getImageWidth());
-        Rasters rasters = fileDirectory.readRasters();
+        byteBuffer.rewind(); 
+        while (byteBuffer.hasRemaining())
+        {
+             values.add(byteBuffer.getFloat());
+        }
+        
+        System.out.println(Collections.max(values));
+        //System.out.println(Collections.min(values));
+        values.remove(Collections.min(values));
+        System.out.println(Collections.min(values));
+        System.out.println(fileDirectory.numEntries());
+        
         
         System.out.println(fileDirectory.getEntries().size());
-        System.out.println(rasters.getPixel(0, 0)[0]);
+        Set<FileDirectoryEntry> fileDirectories = fileDirectory.getEntries();
+        
+        for(FileDirectoryEntry fileE: fileDirectories) {
+        	
+        	System.out.println(fileE.getFieldTag().toString());
+        	System.out.println(fileE.getValues().toString());
+        }
+        
+        
+        
+        
+       
+        
+        
+        
+        
+        
        
                         
         
